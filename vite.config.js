5a1b7c0ca41d2c6666cd1c9a370c9297a2156b45
Jonathan Rodriguez <jonath.josue@gmail.com>
2023-08-26 13:14:19 +0200

    new changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // base: 'https://devrod.site',/
+//http://127.0.0.1:5173/
+//https://jonybebunio.github.io/la-casa-de-burger
 export default defineConfig({
   plugins: [react()],
   base: 'https://jonybebunio.github.io/la-casa-de-burger',
