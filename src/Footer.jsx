919be530ac1e9c87d7459320a7bba9f942abb578
Jonathan Rodriguez <jonath.josue@gmail.com>
2023-09-02 12:13:48 +0200

    devrod

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import navLogo from "../src/assets/Logo.jpeg";
+import devrod from "../src/assets/devrod.png";
 
-export default function Footer(){
-    return(
-        <footer className='footer'>
-            <p>La Casa de Burger &copy; 2023</p>
-            <div className="centered-content">
-            <img src={navLogo} alt="logo" className="logoFooter" />
-                <p>
-                    Direccion: 700,0 m · Av. Diego Ramírez Pastor, 142<br/>
-                    telefono: 602 59 72 10<br/>
-                    WhatsApp: 602 59 72 10<br/>
-                    Pedidos a domicilio, para llevar<br/>
-                    para reservaciones llamar al telefono o por whatsapp
-                </p>
-            </div>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <div className="footer-content">
+        <div className="logo-container">
+          <img src={navLogo} alt="logo" className="logoFooter" />
+          <a href="https://devrod.site/"><img src={devrod} alt="disenadores web" className="logoFooter" /></a>
+          
+        </div>
+        <div className="info-container">
+          <p>
+            Direccion: 700,0 m · Av. Diego Ramírez Pastor, 142<br />
+            Telefono: 602 59 72 10<br />
+            WhatsApp: 602 59 72 10<br />
+            Pedidos a domicilio, para llevar<br />
+            Para reservaciones, llamar al teléfono o por WhatsApp
+          </p>
+        </div>
+      </div>
+      <p className="copyright">La Casa de Burger &copy; 2023</p>
+    </footer>
+  );
 }
