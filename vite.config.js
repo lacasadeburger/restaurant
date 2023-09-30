fa4b0c968404bf959be747ff9f513e223fad6556
lacasadeburger <lacasadeburger.es@gmail.com>
2023-09-30 14:24:38 +0200

    migration gh

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // base: 'https://devrod.site',/
 //http://127.0.0.1:5173/
-//https://jonybebunio.github.io/la-casa-de-burger
 export default defineConfig({
   plugins: [react()],
   base: 'https://lacasadeburger.es',
