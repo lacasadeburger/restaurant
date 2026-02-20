61461995c6648dd194ada2301c6b9a55436457bf
la casa de burger <ton-email@exemple.com>
2026-02-20 17:19:26 +0100

    Update Performance LCP

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
   base: '/',
   build: {
     emptyOutDir: true,
-    minify: 'esbuild',
-    reportCompressedSize: false
-  }
+    // Cela garantit que les assets sont bien gérés
+    assetsDir: 'assets',
+  },
 })
