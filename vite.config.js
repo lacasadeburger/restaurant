775e9d55af30772db5b76a70114d37ce1dd321c5
la casa de burger <ton-email@exemple.com>
2026-02-20 17:10:02 +0100

    Update Performance LCP

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,18 @@ import react from '@vitejs/plugin-react-swc'
 import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [
+    react(),
+    viteSingleFile({
+      removeViteModuleLoader: true,
+      // On empêche d'inclure les fichiers trop gros dans le HTML
+      inlinePattern: ["*.css"],
+      deleteInlinedFiles: true
+    })
+  ],
   base: '/',
   build: {
     emptyOutDir: true,
-    minify: 'esbuild',
-    reportCompressedSize: false
+    assetsInlineLimit: 4096 // Ne transforme en texte que les fichiers < 4ko
   }
 })
