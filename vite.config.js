315cbb14f21e086037667df84a3212600c1fbc38
la casa de burger <ton-email@exemple.com>
2026-02-20 17:05:00 +0100

    Update Performance LCP

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
-// base: 'https://devrod.site',/
-//http://127.0.0.1:5173/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   base: '/',
+  build: {
+    emptyOutDir: true,
+    minify: 'esbuild',
+    reportCompressedSize: false
+  }
 })
