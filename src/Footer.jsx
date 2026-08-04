e188f04674d1c566d9afa655990f25dfc4d0b89a
lacasadeburger <146317961+lacasadeburger@users.noreply.github.com>
2026-08-04 20:03:53 +0200

    Fix formatting in Footer component

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -143,7 +143,7 @@ export default function Footer({ lang }) {
           margin: '0',
           fontWeight: '500'
         }}>
-          LA CASA DE BURGER &copy; {currentYear} | Torrevieja | Sitio web optimizado por <a href="https://localpatron.app" target="_blank" rel="noopener noreferrer" style={{ color: '#8bdbdb', textDecoration: 'underline' }}>LocalPatron</a>
+        LA CASA DE BURGER &copy; {currentYear} | Torrevieja | Sitio web optimizado por <a href="https://localpatron.app" target="_blank" rel="noopener noreferrer" style={{ color: '#8bdbdb', textDecoration: 'underline' }}>LocalPatron</a>
         </p>
 
         <p style={{
